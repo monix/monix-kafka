89486fb42e56b82326fba103773e9e0ec4660a04
Mihai Soloi <mihai.soloi@gmail.com>
2018-02-07 11:06:03 +0200

    upgraded monix dependency to 3.0.0-M3, updated deprecated SingleAssignmentCancelable (#31)

--- kafka-0.10.x/src/main/scala/monix/kafka/KafkaProducer.scala
+++ kafka-0.10.x/src/main/scala/monix/kafka/KafkaProducer.scala
@@ -19,7 +19,7 @@ package monix.kafka
 import com.typesafe.scalalogging.StrictLogging
 import monix.eval.Task
 import monix.execution.atomic.Atomic
-import monix.execution.cancelables.SingleAssignmentCancelable
+import monix.execution.cancelables.SingleAssignCancelable
 import monix.execution.{Cancelable, Scheduler}
 import org.apache.kafka.clients.producer.{Callback, ProducerRecord, RecordMetadata, KafkaProducer => ApacheKafkaProducer}
 
@@ -75,7 +75,7 @@ object KafkaProducer {
           }
           else {
             val isActive = Atomic(true)
-            val cancelable = SingleAssignmentCancelable()
+            val cancelable = SingleAssignCancelable()
             context.connection.push(cancelable)
 
             try {
