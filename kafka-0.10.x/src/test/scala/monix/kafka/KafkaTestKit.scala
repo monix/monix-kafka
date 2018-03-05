056ae082c73d29da86e171dcb82e574a1a52df75
Leandro Bolivar <leandrob131@gmail.com>
2018-03-04 21:44:24 -0500

    Added KafkaTestKit trait for starting embedded kafka for tests in 10, 11,1x

---
+++ kafka-0.10.x/src/test/scala/monix/kafka/KafkaTestKit.scala
@@ -0,0 +1,20 @@
+package monix.kafka
+
+import net.manub.embeddedkafka.EmbeddedKafka
+import org.scalatest.{BeforeAndAfterAll, Suite}
+
+
+trait KafkaTestKit extends BeforeAndAfterAll { self: Suite =>
+
+  override def beforeAll(): Unit = {
+    EmbeddedKafka.start()
+    sys.addShutdownHook {
+      EmbeddedKafka.stop()
+    }
+  }
+
+  override def afterAll(): Unit = {
+    EmbeddedKafka.stop()
+  }
+
+}
