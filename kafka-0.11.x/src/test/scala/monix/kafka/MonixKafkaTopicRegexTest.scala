1669efffdaf9bbb6cf4e693fe55fe0d3d66e51b2
Avasil <pgawrys2@gmail.com>
2018-09-24 14:28:57 +0200

    Update copyright headers

--- kafka-0.11.x/src/test/scala/monix/kafka/MonixKafkaTopicRegexTest.scala
+++ kafka-0.11.x/src/test/scala/monix/kafka/MonixKafkaTopicRegexTest.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2016 by its authors. Some rights reserved.
+ * Copyright (c) 2014-2018 by its authors. Some rights reserved.
  * See the project homepage at: https://github.com/monixio/monix-kafka
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
