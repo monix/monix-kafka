32ed63fab6a1da6a53f1793e81b4b11bf9c24145
Avasil <pgawrys2@gmail.com>
2019-01-15 20:44:41 +0100

    fix error in kafka-9 tests

--- kafka-0.9.x/src/test/scala/monix/kafka/MonixKafkaTest.scala
+++ kafka-0.9.x/src/test/scala/monix/kafka/MonixKafkaTest.scala
@@ -95,7 +95,7 @@ class MonixKafkaTest extends FunSuite {
       .map(_.value())
       .toListL
 
-    val (result, _) = Await.result(Task.parZip2(listT.executeAsync, pushT.executeAsync).runAsync, 60.seconds)
+    val (result, _) = Await.result(Task.parZip2(listT.executeAsync, pushT.executeAsync).runToFuture, 60.seconds)
     assert(result.map(_.toInt).sum === (0 until count).sum)
   }
 }
